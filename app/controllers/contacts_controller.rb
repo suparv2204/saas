37406e5c805f84082bb92727c87354858637d0d9
Suparv Sharma <suparv2204@gmail.com>
2017-06-30 21:38:18 +0000

    Added code comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,23 +1,39 @@
 class ContactsController < ApplicationController
- def new
-   @contact = Contact.new
- end
+  
+  # GET request to /contact-us
+  # Show new contact form
+  def new
+    @contact = Contact.new
+  end
  
- def create
-   @contact = Contact.new(contact_params)
-   if @contact.save
-     name = params[:contact][:name]
-     email = params[:contact][:email]
-     body = params[:contact][:comments] 
-     ContactMailer.contact_email(name, email, body).deliver
-     flash[:success] = "Message sent"
-     redirect_to new_contact_path
-   else
-     flash[:danger] = @contact.errors.full_messages.join(", ")
-     redirect_to new_contact_path
-   end
- end
- private
+  # POST request to /contacts 
+  def create
+    
+    # Mass assignment of form fields into Contact object
+    @contact = Contact.new(contact_params)
+    # Save the Contact object to database
+    if @contact.save
+      # Store form fields via parameters, into variables
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:comments] 
+      
+      # Plug variables into Contact Mailer email method and send email
+      ContactMailer.contact_email(name, email, body).deliver
+      # Store success message in flash hash
+      # and redirect to the new action
+      flash[:success] = "Message sent"
+      redirect_to new_contact_path
+    else
+      # if contact object doesn't save store errors in the flash hash and redirect to new action
+      flash[:danger] = @contact.errors.full_messages.join(", ")
+      redirect_to new_contact_path
+    end
+  end
+  
+private
+  # To collect data from form we need to use strong parameters
+  # and whitelist form fields
   def contact_params
     params.require(:contact).permit(:name, :email, :comments)
   end
